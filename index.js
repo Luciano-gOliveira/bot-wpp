fb8818f44dde5870db3ee0cf289a5205f8aceda7
DeVision <146596775+Luciano-gOliveira@users.noreply.github.com>
2024-12-29 15:56:36 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,23 +4,23 @@ const qrcode = require("qrcode-terminal");
 //TO DO: fazer com que a extensão rode sem necessidade de QRCODE
 const client = new Client();
 
-client.on("ready", () => {
-  console.log("Client is ready!");
+client.on('ready', () => {
+    console.log('Client is ready!');
 });
 
-client.on("ready", () => {
-  console.log("Client is ready!");
-});
-
-client.on("qr", (qr) => {
-  qrcode.generate(qr, { small: true });
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
 });
 
 client.initialize();
 
 client.on("message", (message) => {
     //TO DO: adicionar funcionalidade de envio à botão do whatsapp que recebe os agrupamentos de mensagens
-      client.sendMessage(message.from, `${messagegroup.id}`);
+      // client.sendMessage(message.from, `${messagegroup.id}`);
+      if (message.body === 'ping') {
+  		// responde a mensagem "ping" com "PONG"
+  		message.reply('pong');
+	}
   });
 
 //   forEach([
