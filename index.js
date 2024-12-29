ac9a62de4341f5dd8649491abdb0fb3757752d82
Luciano Oliveira <aulascursos66@gmail.com>
2024-12-28 22:45:05 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const { Client } = require("whatsapp-web.js");
+const qrcode = require("qrcode-terminal");
+
+//TO DO: fazer com que a extensão rode sem necessidade de QRCODE
+const client = new Client();
+
+client.on("ready", () => {
+  console.log("Client is ready!");
+});
+
+client.on("ready", () => {
+  console.log("Client is ready!");
+});
+
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.initialize();
+
+client.on("message", (message) => {
+    //TO DO: adicionar funcionalidade de envio à botão do whatsapp que recebe os agrupamentos de mensagens
+      client.sendMessage(message.from, `${messagegroup.id}`);
+  });
+
+//   forEach([
+//     {
+//         id: 322114423,
+//         name: "Grupo 1",
+//         file: "C://dkdaskjasd"        
+//     },
+//     {
+//         id: 322114423,
+//         name: "Grupo 1",
+//         file: "C://dkdaskjasd"        
+//     },
+//     {
+//         id: 322114423,
+//         name: "Grupo 1",
+//         file: "C://dkdaskjasd"        
+//     },
+//   ]
